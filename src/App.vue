355d49892b519c3ece1dc80cdd6e42c9eddc42f0
iriahk89 <iriahk89@gmail.com>
2024-05-04 17:20:09 +0000

    added per-user data isolation

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,9 @@ import Todos from './components/Todos.vue'
 
 <template>
   <main>    
-    <authenticator>
-      <template v-slot="{ signOut }">
+    <authenticator>    
+      <template v-slot="{ user, signOut }">
+        <h1>Hello {{user?.signInDetails?.loginId}}'s todos</h1>
         <Todos />
         <button @click="signOut">Sign Out</button>
       </template>
